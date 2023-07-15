8bdb88b4d7e9a26cdcd725ff98b6500a985a06c6
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-07-15 21:23:36 +0530

    📦 NEW: Tweet Modal Added and controller added

--- twitter_clone/lib/features/tweet/controller/tweet_controller.dart
+++ twitter_clone/lib/features/tweet/controller/tweet_controller.dart
@@ -1,13 +1,20 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, unused_element
 
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/core/core.dart';
+import 'package:twitter_clone/core/enums/tweet_type_enum.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
+import 'package:twitter_clone/models/tweet_model.dart';
 
 class TweetController extends StateNotifier<bool> {
-  TweetController() : super(false);
+  final Ref _ref;
+  TweetController({
+    required Ref ref,
+  })  : _ref = ref,
+        super(false);
 
   void shareTweet({
     required List<File> images,
@@ -32,10 +39,29 @@ class TweetController extends StateNotifier<bool> {
     required String tweet_text,
     required BuildContext context,
   }) {}
+
   void _shareTweet({
     required String tweet_text,
     required BuildContext context,
-  }) {}
+  }) {
+    state = true;
+    final hashtags = _getHashTagsFromText(tweet_text);
+    String link = _getLinkFromText(tweet_text);
+    final user = _ref.read(currentUserDetailsProvider).value!;
+
+    Tweet tweet = Tweet(
+        text: tweet_text,
+        hashtags: hashtags,
+        link: link,
+        imageLinks: [],
+        uid: user.uid,
+        tweetType: TweetType.text,
+        tweetedAt: DateTime.now(),
+        likes: [],
+        commentIds: [],
+        id: '',
+        reshareCount: 0);
+  }
 
   String _getLinkFromText(String text) {
     final words = text.split(" ");
